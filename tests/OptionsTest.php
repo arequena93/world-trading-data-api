01da67ad21173f0f3e85fb66e23d91fc5ebf3132
antonirs <antoni.requena@gmail.com>
2019-08-28 00:08:00 +0200

    Adding testing for Options class

---
+++ tests/OptionsTest.php
@@ -0,0 +1,27 @@
+<?php
+
+use WorldTradingData\Options;
+
+class OptionsTest extends \PHPUnit\Framework\TestCase
+{
+
+    public function testApiKey()
+    {
+        $option = new Options();
+        $this->assertEmpty($option->getApiKey());
+
+        $randomString = (string)rand(100,156489);
+        $option->setApiKey($randomString);
+        $this->assertSame($randomString, $option->getApiKey());
+    }
+
+    public function testApiUrl()
+    {
+        $option = new Options();
+        $this->assertNotEmpty($option->getApiUrl());
+
+        $randomString = (string)rand(100,156489);
+        $option->setApiUrl($randomString);
+        $this->assertSame($randomString, $option->getApiUrl());
+    }
+}
