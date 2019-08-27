23621c789ad9c4001dfab31c0a71bd72ee57af69
antonirs <antoni.requena@gmail.com>
2019-08-27 14:29:00 +0200

    Adding exception classes

---
+++ src/Exception/BadMethodCallException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace WorldTradingData\Exception;
+
+/**
+ * Class BadMethodCallException
+ * @package WorldTradingData\Exception
+ */
+class BadMethodCallException extends \BadMethodCallException
+{
+
+}
---
+++ src/Exception/InvalidArgumentException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace WorldTradingData\Exception;
+
+/**
+ * Class InvalidArgumentException
+ * @package WorldTradingData\Exception
+ */
+class InvalidArgumentException extends \InvalidArgumentException
+{
+
+}
